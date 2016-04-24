c87b962a19378693a1f04cee4a5a672818ea60ee
luca998 <luca.caracciolo16@gmail.com>
2016-04-24 10:17:49 +0200

    maggiore.c

---
+++ maggiore.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a, b, c, d, e, max, min;
+
+  printf("Inserisci il primo numero: ");
+  scanf("%d", &a);
+  printf("Inserisci il secondo numero: ");
+  scanf("%d", &b);
+  printf("Inserisci il terzo numero: ");
+  scanf("%d", &c);
+  printf("Inserisci il quarto numero: ");
+  scanf("%d", &d);
+  printf("Inserisci il quinto numero: ");
+  scanf("%d", &e);
+
+  max = a;
+  if (b > max) {
+    max = b;}
+  else 
+    min=b;
+  
+  if (c > max) {
+    max = c;}
+  else 
+    min=c;
+  
+  if (d > max) {
+    max = d;}
+  else 
+    min=d;
+  
+  if (e > max) {
+    max = e; }
+  else 
+    min=e;
+ 
+  
+  printf("Il numero maggiore e': %d\n", max);
+  printf("Il numero minore e': %d\n", min);
+  return 0;
+}
